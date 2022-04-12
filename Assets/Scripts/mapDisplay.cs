10000ed0435349a5cdb7a46bf5b119761482f2e7
ZanxiangWang <zwang236@ncsu.edu>
2022-04-12 18:16:04 -0400

    map generation testing fly testing

--- Assets/Scripts/mapDisplay.cs
+++ Assets/Scripts/mapDisplay.cs
@@ -6,14 +6,14 @@ public class mapDisplay : MonoBehaviour
 {
     public Renderer textureRender;
 
-    public boid DrawNoiseMap(float[,], noiseMap)
+    public void DrawNoiseMap(float[,] noiseMap)
     {
         int width = noiseMap.GetLength (0);
         int height = noiseMap.GetLength (1);
 
         Texture2D texture = new Texture2D(width, height);
 
-        Color[] colourMap = new Color[wieth * height];
+        Color[] colourMap = new Color[width * height];
         for(int y = 0; y < height; y++)
         {
             for(int x = 0; x < width; x++)
@@ -24,7 +24,7 @@ public class mapDisplay : MonoBehaviour
         texture.SetPixels(colourMap);
         texture.Apply();
 
-        textureRender.ShareMaterial.mainTexture = texture;
+        textureRender.sharedMaterial.mainTexture = texture;
         textureRender.transform.localScale = new Vector3(width, 1, height);
     }
 }
